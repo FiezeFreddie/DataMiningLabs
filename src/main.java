c6dc21b1a71b9b1925a9b1b600c5dc16e2a782d7
mvansmaalen <M.F.vanSmaalen@student.tudelft.nl>
2018-03-01 23:32:12 +0100

    added files

---
+++ src/main.java
@@ -0,0 +1,31 @@
+import java.util.List;
+import java.util.Set;
+
+
+public class main {
+	
+	public static void exercise1_1() {
+		// ADD CODE HERE
+	}
+	
+	public static void exercise1_2() {
+	}
+	
+	public static void exercise1_3(MinHash mh) {
+		MinHash hash = new MinHash();
+		hash.addRandomHashFunctions(100);
+
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// exercise 1.1
+		exercise1_1();
+		
+		// exercise 1.2
+		exercise1_2();
+	}
+
+}
